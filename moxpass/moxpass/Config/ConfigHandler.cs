3c99a4dda6de62a5f01c8c4865a1523f847f34e1
mchenf <mochen@foss.dk>
2023-02-20 17:52:14 +0800

    Added Configuration handler

---
+++ moxpass/moxpass/Config/ConfigHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace moxpass.Config
+{
+    public class ConfigHandler
+    {
+    }
+
+    public class Settings
+    {
+        public string this[string key]
+        {
+            get
+            {
+                return d[key];
+            }
+            set
+            {
+                d[key] = value;
+            }
+        }
+
+        private Dictionary<string, string> d = 
+            new Dictionary<string, string>();
+    }
+}
