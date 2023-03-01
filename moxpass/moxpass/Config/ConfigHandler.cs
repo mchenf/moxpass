2edc1659531108617204b0d1c0dfd5930cbcc03f
mchenf <mochen@foss.dk>
2023-03-01 11:34:27 +0800

    Completed basic configuration handler

--- moxpass/moxpass/Config/ConfigHandler.cs
+++ moxpass/moxpass/Config/ConfigHandler.cs
@@ -14,6 +14,44 @@ namespace moxpass.Config
 
     public class Settings
     {
+        private string settingFilePath;
+        public static readonly string[] Options = new string[]
+        {
+            "User.Email=", "Secret.DisplayTime="
+        };
+        public Settings(string filePath)
+        {
+            this.settingFilePath = filePath;
+            if(!File.Exists(settingFilePath))
+            {
+                string tx = string.Join(Environment.NewLine, Options);
+                byte[] b = Encoding.UTF8.GetBytes(tx);
+                using (var fs = File.Create(settingFilePath))
+                {
+                    fs.Write(b, 0, b.Length);
+                }
+            }
+            string content = File.ReadAllText(settingFilePath);
+            string[] lines = content.Split(Environment.NewLine);
+            foreach (string line in lines)
+            {
+                string[] kvp = line.Split('=');
+
+                switch (kvp.Length)
+                {
+                    case 1:
+                        this[kvp[0]] = "";
+                        break;
+                    case 2:
+                        this[kvp[0]] = kvp[1];
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+        }
         public string this[string key]
         {
             get
@@ -28,5 +66,22 @@ namespace moxpass.Config
 
         private Dictionary<string, string> d = 
             new Dictionary<string, string>();
+
+        public void Save()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int len = d.Count();
+
+            foreach (var kvp in d)
+            {
+                sb.Append(kvp.Key);
+                sb.Append("=");
+                sb.Append(kvp.Value);
+                if(len > 0) sb.AppendLine();
+                len--;
+            }
+            File.WriteAllText(settingFilePath, sb.ToString());
+        }
     }
 }
---
+++ moxpass/moxpassTests/Config/SettingsTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using moxpass.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moxpass.Config.Tests
+{
+    [TestClass()]
+    public class SettingsTests
+    {
+        [TestMethod()]
+        [DataRow(@"c:\users\mochen\moxpass.setting")]
+        public void SettingsTest(string arg1)
+        {
+            var s = new Settings(arg1);
+
+            Console.WriteLine(File.ReadAllText(arg1));
+            Assert.IsTrue(true);
+        }
+
+
+        [TestMethod()]
+        [DataRow(@"c:\users\mochen\moxpass.setting", "", "")]
+        public void SettingsReadTest(string arg1, string exp1, string exp2)
+        {
+            var s = new Settings(arg1);
+
+            Assert.AreEqual(exp1, s["User.Email"]);
+            Assert.AreEqual(exp2, s["Secret.DisplayTime"]);
+        }
+
+        [TestMethod()]
+        [DataRow(@"c:\users\mochen\moxpass.setting", "joedoe@company.com", "12s")]
+        public void SettingsSaveTest(string arg1, string arg2, string arg3)
+        {
+            var s = new Settings(arg1);
+
+            s["User.Email"] = arg2;
+            s["Secret.DisplayTime"] = arg3;
+
+            s.Save();
+
+            Console.WriteLine(File.ReadAllText(arg1));
+
+            Assert.IsTrue(true);
+        }
+
+
+    }
+}
