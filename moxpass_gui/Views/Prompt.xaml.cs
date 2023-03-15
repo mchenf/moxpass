5694a3c9cc263838390670848a5e36aa14385cea
mchenf <mochen@foss.dk>
2023-03-15 14:30:06 +0100

    Made better login gui and binding to the email

--- moxpass_gui/Views/Prompt.xaml.cs
+++ moxpass_gui/Views/Prompt.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using moxpass_gui.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,16 +21,57 @@ namespace moxpass_gui.Views
     /// </summary>
     public partial class Prompt : Window
     {
+        public string Email
+        {
+            get { return (string)GetValue(EmailProperty); }
+            set { SetValue(EmailProperty, value); }
+        }
 
-        private PromptViewModel viewModel = new PromptViewModel();
+        // Using a DependencyProperty as the backing store for Email.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EmailProperty =
+            DependencyProperty.Register("Email", typeof(string), typeof(Prompt), new PropertyMetadata(string.Empty));
+
+
+
+        public bool PasswordBoxReady
+        {
+            get { return (bool)GetValue(PasswordBoxReadyProperty); }
+            set { SetValue(PasswordBoxReadyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PasswordBoxReady.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PasswordBoxReadyProperty =
+            DependencyProperty.Register("PasswordBoxReady", typeof(bool), typeof(Prompt), new PropertyMetadata(false));
+
+
+
         public Prompt()
         {
             InitializeComponent();
+            PassBoxTitle.PasswordChanged += PassBoxTitle_PasswordChanged;
+        }
+
+        private void PassBoxTitle_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if(PassBoxTitle.SecurePassword.Length > 6)
+            {
+                PasswordBoxReady = true;
+            } 
+            else 
+            { 
+                PasswordBoxReady = false; 
+            }
         }
 
         public void CancelClicked(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        public void LoginClicked(object sender, RoutedEventArgs e)
+        {
+            Debug.Print("The email is:");
+            Debug.Print(Email);
         }
     }
 }
